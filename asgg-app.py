7a336f0b658e8f9870706076efdbe4e4906d3e9e
ajnengineeringintl-webagency <ajnengineeringintl@gmail.com>
2026-08-20 15:45:34 -0700

    c

--- asgg-app.py
+++ asgg-app.py
@@ -2,6 +2,7 @@ from app import asgg_app_server
 import os
 
 '''PRIASE THE LORD'''
-asgg_app = asgg_app_server()
+app = asgg_app_server()
+asgg_app = app
 if __name__ == "__main__":
-    asgg_app.run(debug=True,port=os.environ.get("ASGG_APP_PORTNUMBER"))
+    app.run(debug=True,port=os.environ.get("ASGG_APP_PORTNUMBER"))
